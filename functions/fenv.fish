de14458fb472d2832110df88dc5e2a5d9c12e684
ash <ash@sorrel.sh>
2023-03-28 18:52:25 +0100

    Don't make the entire fish function a no-op
    why am i like this

--- functions/fenv.fish
+++ functions/fenv.fish
@@ -1,6 +1,6 @@
 function fenv
   set -l assignments (_fenv $argv | string split0)
   for idx in (seq 1 2 (count $assignments))
-    echo set -gx "$assignments[$idx]" "$assignments[$(math $idx + 1)]"
+    set -gx "$assignments[$idx]" "$assignments[$(math $idx + 1)]"
   end
 end
