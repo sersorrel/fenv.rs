9f7ee64a8131ae1eb9c5e66834cbb02d5f6526a8
ash <ash@sorrel.sh>
2023-07-12 22:33:10 +0100

    Silence error on macOS if no variables are set
    BSD seq complains ("seq: needs negative decrement") if last is less than
    first and increment is nonnegative (and not implicit).

--- functions/fenv.fish
+++ functions/fenv.fish
@@ -1,6 +1,9 @@
 function fenv
   set -l assignments (_fenv $argv | string split0)
-  for idx in (seq 1 2 (count $assignments))
-    set -gx "$assignments[$idx]" "$assignments[$(math $idx + 1)]"
+  set -l n (count $assignments)
+  if test $n -ne 0
+    for idx in (seq 1 2 $n)
+      set -gx "$assignments[$idx]" "$assignments[$(math $idx + 1)]"
+    end
   end
 end
